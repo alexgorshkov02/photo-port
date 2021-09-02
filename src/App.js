5c2ab038ea6752184fa59dd3c1a9cdae561d63df
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-09-02 00:34:43 -0400

    Code refactoring

--- src/App.js
+++ src/App.js
@@ -16,26 +16,27 @@ function App() {
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   const [contactSelected, setContactSelected] = useState(false);
-  
+
   return (
     <div>
       <Nav
-  categories={categories}
-  setCurrentCategory={setCurrentCategory}
-  currentCategory={currentCategory}
-  contactSelected={contactSelected}
-  setContactSelected={setContactSelected}
-></Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Nav>
       <main>
         {!contactSelected ? (
-  <>
-    <Gallery currentCategory={currentCategory}></Gallery>
-    <About></About>
-  </>
-) : (
-    <ContactForm></ContactForm>
-  )}
+          <>
+            <Gallery currentCategory={currentCategory}></Gallery>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
